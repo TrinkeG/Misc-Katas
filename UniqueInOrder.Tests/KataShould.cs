30044242043c62cf18a7e674403284fdeaf63081
Christina Ripley-Gonzalez <christina.ripley@codurance.com>
2018-01-04 11:26:04 +0000

    Unique In Order

---
+++ UniqueInOrder.Tests/KataShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UniqueInOrder.Tests
+{
+    [TestFixture]
+    public class KataShould
+    {
+
+        [Test]
+        public void return_empty_string_for_empty_input()
+        {
+            Assert.AreEqual("", Kata.UniqueInOrder(""));
+        }
+
+        [Test]
+        public void return_single_characters_for_character_input()
+        {
+            Assert.AreEqual("ABCDAB", Kata.UniqueInOrder("AAAABBBCCDAABBB"));
+        }
+    }
+}
---
+++ UniqueInOrder/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace UniqueInOrder
+{
+    public class Kata
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            if (!iterable.Any()) return iterable;
+            var items = new List<T> {iterable.First()};
+            foreach (var item in iterable.Skip(1))
+            {
+                if (!item.Equals(items.Last()))
+                {
+                    items.Add(item);
+                }
+            }
+            return items;
+        }
+    }
+}
